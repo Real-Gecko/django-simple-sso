16597aaedf499a4401b6fb3db783faf535b10820
RealGecko <alexandrbezenkov@gmail.com>
2024-12-12 04:14:56 +0500

    Update `default_auto_field` to avoid unnecessary migrations

--- simple_sso/sso_server/apps.py
+++ simple_sso/sso_server/apps.py
@@ -3,8 +3,10 @@ from django.contrib.auth.signals import user_logged_out
 
 
 class SimpleSSOServer(AppConfig):
-    name = 'simple_sso.sso_server'
+    name = "simple_sso.sso_server"
+    default_auto_field = "django.db.models.AutoField"
 
     def ready(self):
         from simple_sso.signals import logout_token
-        user_logged_out.connect(logout_token, dispatch_uid='logout_token')
+
+        user_logged_out.connect(logout_token, dispatch_uid="logout_token")

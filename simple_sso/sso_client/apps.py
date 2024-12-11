a67cd13b0563e06ebb94f6cdab40952f3a60a04e
RealGecko <alexandrbezenkov@gmail.com>
2024-12-12 04:44:51 +0500

    Enable `sso_client` without `sso_server` models

--- simple_sso/sso_client/apps.py
+++ simple_sso/sso_client/apps.py
@@ -3,9 +3,9 @@ from django.contrib.auth.signals import user_logged_out
 
 
 class SimpleSSOClient(AppConfig):
-    name = 'simple_sso.sso_client'
+    name = "simple_sso.sso_client"
 
     def ready(self):
-        from simple_sso.signals import logout_sso_client
-        user_logged_out.connect(
-            logout_sso_client, dispatch_uid='logout_sso_client')
+        from simple_sso.sso_client.signals import logout_sso_client
+
+        user_logged_out.connect(logout_sso_client, dispatch_uid="logout_sso_client")
---
+++ simple_sso/sso_client/signals.py
@@ -0,0 +1,29 @@
+from django.urls import NoReverseMatch, reverse
+from webservices.sync import SyncConsumer
+
+from simple_sso.settings import settings
+
+
+def logout_sso_client(sender, request, user, **kwargs):
+    """
+    A signal receiver which notifies the server when the client have logged out.
+    """
+    access_token = request.session.get("sso_access_token", None)
+
+    if access_token is not None:
+        consumer = SyncConsumer(
+            settings.SSO_SERVER_URL, settings.SSO_PUBLIC_KEY, settings.SSO_PRIVATE_KEY
+        )
+
+        try:
+            url = reverse("simple-sso-logout")
+        except NoReverseMatch:
+            # this is fine
+            url = "/logout/"
+
+        data = {"access_token": access_token}
+        try:
+            consumer.consume(url, data)
+        except Exception:
+            # Ignore errors for now
+            pass
